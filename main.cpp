fbdffa52d3775298ed64456988a67ad00b26d153
Shadow-nx <32244138+Shadow-nx@users.noreply.github.com>
2017-09-29 23:15:44 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
-#include <iostream> 
+#include <iostream>
+#include <cstdlib>
+using namespace std;
 
-int main()
+int main(int argc, char*argv[])
 {
 
+	
+if((char)argv[2][0]=='+'&&(isdigit(argv[3][0])&&isdigit(argv[1][0])))	
+	cout<<atoi(argv[1])+atoi(argv[3])<<endl;
+else if((char)argv[2][0]=='-'&&(isdigit(argv[3][0])&&isdigit(argv[1][0])))	
+    cout<<atoi(argv[1])-atoi(argv[3])<<endl;
+else cerr<<"ERROR";  
+
+
+return 0;	
 }
